2612e6b0a536db5501d381c4fb90ea60eef80f93
Marco Platzer <mail@latzo.ch>
2024-07-03 13:20:53 +0200

    Add function_name

--- function_app.py
+++ function_app.py
@@ -9,6 +9,7 @@ from azure.cosmosdb.table.models import Entity
 
 app = func.FunctionApp()
 
+@app.function_name(name="update_counter")
 @app.route(route="update_counter", auth_level=func.AuthLevel.FUNCTION)
 def update_counter(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
